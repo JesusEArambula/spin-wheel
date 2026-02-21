1eb45adba1cb9a59ed2d52f70e8718ea9ac55387
Jesus Arambula <61527697+JesusEArambula@users.noreply.github.com>
2026-02-20 17:29:38 -0800

    Added confetti! 🎊

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 let wheel = document.querySelector('.wheel');
 let button = document.querySelector('.button');
+let confetti = document.querySelector('#confetti');
+const blowConfetti = new JSConfetti();
+
 let value = Math.ceil(Math.random() * 3600);
 
 button.onclick = function() {
     wheel.style.transform = "rotate(" + value + "deg)";
     value += Math.ceil(Math.random() * 3600);
+    setTimeout(() => {
+        blowConfetti.addConfetti();
+        blowConfetti.addConfetti();
+    }, 4000)
 }
